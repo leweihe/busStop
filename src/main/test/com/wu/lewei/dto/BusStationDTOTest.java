073bb6980d9389336554ed7e30e90d3bc7911e26
helewei <Lewei.He@linde-china.com>
2017-03-02 16:35:49 +0800

    optimize the name of the station

--- src/main/test/com/wu/lewei/dto/BusStationDTOTest.java
+++ src/main/test/com/wu/lewei/dto/BusStationDTOTest.java
@@ -25,14 +25,14 @@ public class BusStationDTOTest {
     @Inject
     StationRepository stationRepo;
 
-    @Before
-    public void before() {
+    @Test
+    public void cleardata() {
         busRouteRepo.deleteAll();
         stationRepo.deleteAll();
     }
 
     @Test
-    public void test() {
+    public void initNo1() {
         BusRouteDTO busRoute = new BusRouteDTO();
         busRoute.setRouteName("No.1");
         busRoute.setDescription("思北");
@@ -68,6 +68,55 @@ public class BusStationDTOTest {
         stationRepo.save(Arrays.asList(s1, s2, s3, s4, s5, s6, s7));
 
         busRoute.setStations(Arrays.asList(s1, s2, s3, s4, s5, s6, s7));
+
+        busRouteRepo.save(busRoute);
+    }
+
+    @Test
+    public void initNo2() {
+        BusRouteDTO busRoute = new BusRouteDTO();
+        busRoute.setRouteName("No.2");
+        busRoute.setDescription("轮渡");
+
+        BusStationDTO s1 = new BusStationDTO();
+        s1.setKeyword("角滨路口");
+        s1.setCity("厦门");
+
+        BusStationDTO s2 = new BusStationDTO();
+        s2.setCity("厦门");
+        s2.setKeyword("中山医院");
+
+        BusStationDTO s3 = new BusStationDTO();
+        s3.setCity("厦门");
+        s3.setKeyword("湖滨南 新村");
+
+        BusStationDTO s4 = new BusStationDTO();
+        s4.setCity("厦门");
+        s4.setKeyword("自行车厂");
+
+        BusStationDTO s5 = new BusStationDTO();
+        s5.setCity("厦门");
+        s5.setKeyword("槟榔西里");
+
+        BusStationDTO s6 = new BusStationDTO();
+        s6.setCity("厦门");
+        s6.setKeyword("槟榔东");
+
+        BusStationDTO s7 = new BusStationDTO();
+        s7.setCity("厦门");
+        s7.setKeyword("常明路口");
+
+        BusStationDTO s8 = new BusStationDTO();
+        s8.setCity("厦门");
+        s8.setKeyword("常明路口");
+
+        BusStationDTO s9 = new BusStationDTO();
+        s9.setCity("厦门");
+        s9.setKeyword("莲花三村");
+
+        stationRepo.save(Arrays.asList(s1, s2, s3, s4, s5, s6, s7, s8, s9));
+
+        busRoute.setStations(Arrays.asList(s1, s2, s3, s4, s5, s6, s7, s8, s9));
 
         busRouteRepo.save(busRoute);
     }
